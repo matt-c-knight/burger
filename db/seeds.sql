9c4c899f7158ed1a79e4cbe8af43cc12ab133bb1
Matthew Knight <mattknight.sa@gmail.com>
2020-01-19 18:32:46 -0600

    set up server sql config

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeze', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie', true);
