8e9a830771d3b3e96c39bc91671bae4e95a99294
Matthew Knight <mattknight.sa@gmail.com>
2020-01-21 13:22:51 -0600

    update model

--- models/burger.js
+++ models/burger.js
@@ -5,6 +5,16 @@ var burger = {
       orm.selectAll("burgers", function(res) {
         cb(res);
       });
+    },
+    create: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res){
+            cb(res);
+        });
+    },
+    update: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
     }
 }
 
